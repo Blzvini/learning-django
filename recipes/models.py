7a2fd6826127041646421c8436e2a44ab3131c87
Blzvini <lopes.viniciusm99@gmail.com>
2023-10-08 23:25:20 -0300

    Corrigindo nome de uma coluna no banco de dados, e adicionando mais algumas receitas

--- recipes/models.py
+++ recipes/models.py
@@ -5,11 +5,14 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=65)
 
+    def __str__(self):
+        return self.name
+
 
 class Recipe(models.Model):
     title = models.CharField(max_length=65)
     description = models.CharField(max_length=165)
-    slub = models.SlugField()
+    slug = models.SlugField()
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
@@ -23,3 +26,6 @@ class Recipe(models.Model):
     category = models.ForeignKey(
         Category, on_delete=models.SET_NULL, null=True)
     author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return self.title
